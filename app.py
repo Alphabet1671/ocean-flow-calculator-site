5ec6667efe36f0e151d1b85f304ee58b13d89ff8
Alphabet1671 <23yel@shadysideacademy.org>
2022-06-25 18:35:05 -0700

    added about us and contact page to flask code

--- app.py
+++ app.py
@@ -9,16 +9,27 @@ def index():
 
 
 @app.route("/fill-form/", methods=["POST", "GET"])
-def fillData():
+def fill_data():
     lat = request.form["latitude"]
     lon = request.form["longitude"]
     month = request.form["month"]
     date = request.form["date"]
 
-    #back end programming here
+    # back end programming here
     f = open("translated avg.csv", "r")
 
 
-#for local testing on editing machine
+@app.route("/about-us/")
+def show_about():
+    return render_template("about-us.html")
+
+
+@app.route("/contact/")
+def show_contact():
+    return render_template("contact.html")
+
+
+# code below is for local testing on editing machine
+
 if __name__ == "__main__":
     app.run(port=2328, host="0.0.0.0", debug=True)
