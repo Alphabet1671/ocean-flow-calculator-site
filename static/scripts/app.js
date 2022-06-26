ebd10fefb6388b228a2d793f84738d8069823cf4
ashlyn <68078477+ashlyn12923190@users.noreply.github.com>
2022-06-26 08:48:38 -0400

    script moving to html file

--- static/scripts/app.js
+++ static/scripts/app.js
@@ -1,11 +1,13 @@
-const img = document.getElementById("map")
-img.onclick = (e) => {
-  const x = e.pageX - e.target.offsetLeft;
-  const y = e.pageY - e.target.offsetTop;
-  console.log(x, y);
+// const img = document.getElementById("map")
+// img.onclick = (e) => {
+//   const x = e.pageX - e.target.offsetLeft;
+//   const y = e.pageY - e.target.offsetTop;
+//   console.log(x, y);
 
-    document.getElementById("lat").value = x;
-    document.getElementById("long").value = y;
-  }
+//     document.getElementById("lat").value = x;
+//     document.getElementById("long").value = y;
+//   }
 
 // still gotta get the conversion to lat and long coords, and since all files aren't in the same folder i might need to just change the route
+
+// ^ still need conversion but script is in html script tags now.
