6b88cb12d4deee51fdc7062966bc330b27cb93e4
Alphabet1671 <23yel@shadysideacademy.org>
2022-06-25 19:58:31 -0700

    tried to add google map stuff but failed

--- app.py
+++ app.py
@@ -9,27 +9,16 @@ def index():
 
 
 @app.route("/fill-form/", methods=["POST", "GET"])
-def fill_data():
+def fillData():
     lat = request.form["latitude"]
     lon = request.form["longitude"]
     month = request.form["month"]
     date = request.form["date"]
 
-    # back end programming here
+    #back end programming here
     f = open("translated avg.csv", "r")
 
 
-@app.route("/about-us/")
-def show_about():
-    return render_template("about-us.html")
-
-
-@app.route("/contact/")
-def show_contact():
-    return render_template("contact.html")
-
-
-# code below is for local testing on editing machine
-
+#for local testing on editing machine
 if __name__ == "__main__":
     app.run(port=2328, host="0.0.0.0", debug=True)
