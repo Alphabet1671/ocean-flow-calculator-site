a2b9140df5bedd621e678b32be8d72928a870c9d
Alphabet1671 <23yel@shadysideacademy.org>
2022-06-25 17:34:27 -0700

    added the csv file for proccessing

--- app.py
+++ app.py
@@ -8,11 +8,15 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/fill-form/", methods=["POST","GET"])
+@app.route("/fill-form/", methods=["POST", "GET"])
 def fillData():
+    lat = request.form["latitude"]
+    lon = request.form["longitude"]
+    month = request.form["month"]
+    date = request.form["date"]
+
     #back end programming here
-
-
+    f = open("translated avg.csv", "r")
 
 
 #for local testing on editing machine
